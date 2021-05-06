38704fd6d4352f0b63d03f315a8a0dd799484b8f
fadime94 <fadime.ozdemir11@gmail.com>
2021-05-06 21:43:52 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const{ graphqlHTTP }= require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require("mongoose")
+const cors = require('cors')
+const MONGO_URL = require('../config');
+const app = express();
+app.use(cors()) 
+mongoose.connect(MONGO_URL,
+{ useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connection.once("open", ()=>
+console.log("connected to db"))
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true // ==> real live fetch + docs 
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
